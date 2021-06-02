ee6cc5a118b82ef47344cb17d46930db3e6218f4
Elias Lima <eliasv.lima@yahoo.com.br>
2021-06-02 18:37:16 -0300

    Limpando estrutura de arquivos no ReactJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React from 'react';
+//import axios from 'axios';
 
 function App() {
-  axios.get('/usuario').then(resultado => {console.log(resultado)});
+ // axios.get('/usuario').then(resultado => {console.log(resultado)});
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Olá Mundo!</h1>
       </header>
     </div>
   );
