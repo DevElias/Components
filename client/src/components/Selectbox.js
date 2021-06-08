45e21b37b45005b0d01c4e0358d2944da4894977
Elias Lima <eliasv.lima@yahoo.com.br>
2021-06-07 23:18:57 -0300

    SelectBox v 1.0

--- client/src/components/Selectbox.js
+++ client/src/components/Selectbox.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from "react";
+import "../css/selectBox.css";
 
-class Selectbox extends React.Component{
-  render(){
-    return <h1>Componente SelectBox</h1>
-  }
+const Auto = () => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    const listagem = [];
+    const promises = new Array(30)
+      .fill()
+      .map((v, i) => fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
+    Promise.all(promises).then(dadosArr => {
+      return dadosArr.map(value =>
+        value
+          .json()
+          .then(({ name, sprites: { front_default: sprite } }) =>
+          listagem.push({ name, sprite })
+          )
+      );
+    });
+    setOptions(listagem);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = event => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const updateValor = valor => {
+    setSearch(valor);
+    setDisplay(false);
+  };
+
+  return (
+    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
+      <input
+        id="auto"
+        onClick={() => setDisplay(!display)}
+        placeholder="Tipo de Busca"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+      />
+      {display && (
+        <div className="autoContainer">
+          {options
+            .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
+            .map((value, i) => {
+              return (
+                <div
+                  onClick={() => updateValor(value.name)}
+                  className="option"
+                  key={i}
+                  tabIndex="0"
+                >
+                  <span>{value.name}</span>
+                  <img src={value.sprite} alt="imagem" />
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Custom SelectBox ReactJS</h1>
+      <div className="logo"></div>
+      <div className="auto-container">
+        <Auto />
+      </div>
+    </div>
+  );
 }
 
-export default Selectbox;
+export default App;
